2c52f9e52d9b0d5157c533aa07329d8582e2e44a
seoin <seoin2389@icloud.com>
2024-03-18 06:12:51 +0900

    Add Security Config - remember-me 추가

--- basicsecurity/src/main/java/io/security/basicsecurity/config/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,6 +13,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -25,6 +29,9 @@ import java.util.logging.Handler;
 @Configuration
 @EnableWebSecurity
 public class  SecurityConfig {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -76,7 +83,18 @@ public class  SecurityConfig {
                                     }
                                 })
                                 .deleteCookies("remeber-me")
-                );
+                )
+                .rememberMe( me ->
+                        me
+                                .rememberMeParameter("remember") // 기본 파라미터명은 remember-me인데 remember로 변경
+                                .tokenValiditySeconds(3600) // 1시간으로 변경 -> default 는 14일
+                                .alwaysRemember(false) // ture면 기능이 활성화되지 않아도 항상 실행 -> 기본 false
+                                .userDetailsService(userDetailsService)// 계정을 조회하는 class
+                )
+
+
+
+        ;
         return http.build();
     }
 
